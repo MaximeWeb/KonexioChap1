34fe5ea83aaed91befc83f9f404af9fa257ccb11
MaximeWeb <Neyret.maxime@gmail.com>
2023-09-26 10:03:45 +0200

    new fichiers

---
+++ index.js
@@ -0,0 +1,50 @@
+const continent = "Europe";
+const country = "France";
+let population = 34 ;
+const isIsland = false;
+let langage = "francais"
+let finland = 6
+const age = 27
+const year = 1996
+const now = 2023 
+const name = "Maxime";
+const userName = "Neyret"
+let x = 10+5
+langage = 'french'
+
+let description = `${country} ,is in ${continent} and its ${population} inhabitants speak ${langage}`
+
+population /= 2
+population ++ 
+
+console.log( population > finland )
+console.log ( population < 33 )
+console.log ( description )
+console.log(continent,country,population)
+console.log(typeof isIsland)
+console.log(langage)
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log( year < now )
+
+
+
+
+
+
+
+
